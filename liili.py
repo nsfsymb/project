f187c9ed9d165a0593a78b66abc5edc8293f08b4
nsfsymb <nsfsymb@gmail.com>
2026-05-05 10:26:27 +0900

    Update liili.py

--- liili.py
+++ liili.py
@@ -36,3 +36,13 @@ region_count = df["지역"].value_counts()
 
 st.subheader("지역별 장소 개수")
 st.bar_chart(region_count)
+
+type_count = df["유형"].value_counts()
+
+st.subheader("유형별 장소 개수")
+st.bar_chart(type_count)
+
+avg_score = df.groupby("지역")["평점"].mean()
+
+st.subheader("지역별 평균 평점")
+st.bar_chart(avg_score)
